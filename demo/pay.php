fdf9830959f5bad4e9071311dc671b0719d62fe6
HuNanZai <123165587@qq.com>
2016-02-18 14:20:14 +0800

    1. 发起支付demo上传

---
+++ demo/pay.php
@@ -0,0 +1,28 @@
+<?php
+include_once __DIR__.'/bootstrap.php';
+\HuNanZai\Component\Log\LoggerFactory::setLogFolderPath(__DIR__.'/log/');
+
+use HuNanZai\Component\Pay\Package\Alipay_wap\Api as AlipayApi;
+use HuNanZai\Component\Pay\Package\Alipay_wap\Handler\PhpFileConfigHandler as ConfigHandler;
+use HuNanZai\Component\Pay\Package\Alipay_wap\Handler\ArrayPayParamHandler as PayParamHandler;
+
+$config_handler     = new ConfigHandler(__DIR__.'/config.php');
+$config = $config_handler->getConfig();
+
+$pay_param_handler  = new PayParamHandler(array(
+    'out_trade_no'  => date('YmdHis'),
+    'subject'       => '测试订单',
+    'total_fee'     => '0.01',
+    //可空
+    'notify_url'    => '',
+    'return_url'    => '',
+));
+
+$alipay_api = new AlipayApi();
+try {
+    $result = $alipay_api->pay($config_handler->getConfig(), $pay_param_handler->getParam());
+} catch (Exception $e) {
+    var_dump($e);
+}
+
+var_dump($result);
